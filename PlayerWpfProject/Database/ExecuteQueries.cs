0926461d56ee8290ba7a9cdb6f86e54ca66ba91b
István Balázs Táborosi <55479420+Ultimateees@users.noreply.github.com>
2022-06-06 09:33:57 +0200

    Basic queries (register, delete, update) for users complete.

--- PlayerWpfProject/Database/ExecuteQueries.cs
+++ PlayerWpfProject/Database/ExecuteQueries.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using PlayerWpfProject.Model;
 
 namespace PlayerWpfProject.Database
 {
     internal class ExecuteQueries
-    {
+    {   
+        private readonly MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;SslMode=none");
+        MySqlDataReader reader = null;
+
+        public void RegisterUser(User user)
+        {
+            string query = $"INSERT INTO wpfplayer.users(username, email, password) VALUES('{user.Username}', '{user.Email}', '{user.Password})';";
+            RunQuery(query);
+        }
+
+        public void DeleteUser(string username)
+        {
+            string query = $"DELETE FROM wpfplayer.users WHERE username='{username}';";
+            RunQuery(query);
+        }
+
+        public void UpdateUser(User user)
+        {
+            string query = $"UPDATE wpfplayer.users SET username='{user.Username}', email='{user.Email}', password='{user.Password}' WHERE username='{user.Username}';";
+            RunQuery(query);
+        }
+
+        public void RunQuery(string query)
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Success!");
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            connection.Close();
+        }
     }
 }
