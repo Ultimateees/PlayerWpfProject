dedc586d31fbcce381d0922fd902020c20eb100f
István Balázs Táborosi <55479420+Ultimateees@users.noreply.github.com>
2022-06-06 08:05:17 +0200

    Player class constructor and properties done

--- PlayerWpfProject/Model/Player.cs
+++ PlayerWpfProject/Model/Player.cs
@@ -8,5 +8,41 @@ namespace PlayerWpfProject.Model
 {
     internal class Player
     {
+        public Player(string name, string pClass, string gender)
+        {
+            Name = name;
+            PClass = pClass;
+            Gender = gender;
+            Health = 1;
+            Damage = 1;
+            Defense = 1;
+            Xp = 1;
+            Level = 1;
+            SkillPoints = 10;
+        }
+
+        public string Name { get; set; }
+        public string PClass { get; set; }
+        public string Gender { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public int Defense { get; set; }
+        public int Id { get; set; }
+        public int Xp { get; set; }
+        public int Level { get; set; }
+        public int SkillPoints { get; set; }
+
+        public override string ToString()
+        {
+            return $"Player name: {Name}"
+                    + $", class: {PClass}"
+                    + $", gender: {Gender}"
+                    + $", health: {Health}"
+                    + $", damage: {Damage}"
+                    + $", defense:{Defense}"
+                    + $", xp:{Xp}"
+                    + $", level:{Level}"
+                    + $", skillpoints:{SkillPoints}";
+        }
     }
 }
